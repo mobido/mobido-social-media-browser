5f9a8211ef85eebed26c56381e7b49277228fe9e
Mike Prince <mike@mikeprince.com>
2023-03-20 18:06:03 -0700

    Renamed to mobidoSocialMediaApp

--- ios/mobidoSocialMediaAppTests/mobidoSocialMediaAppTests.m
+++ ios/mobidoSocialMediaAppTests/mobidoSocialMediaAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobidoSocialMediaBrowserTests : XCTestCase
+@interface mobidoSocialMediaAppTests : XCTestCase
 
 @end
 
-@implementation mobidoSocialMediaBrowserTests
+@implementation mobidoSocialMediaAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
